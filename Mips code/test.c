838b96ea493f2b1e0e1dca74ad724b710b424787
roopasree123 <cs19b008@iittp.ac.in>
2022-04-14 14:36:16 +0530

    mips basics added

--- Mips code/test.c
+++ Mips code/test.c
@@ -12,14 +12,14 @@ if(( fp = fopen("z.dat", "r")) != 0)
  while(fscanf(fp,"%s", a) != EOF)
  {
  i=0;
- if( strlen(a)==6)
+ if( strlen(a)==5)
  {
- i=i+3;
+ i=i+2;
  if(islower(a[i]))
  printf("lw $t%d, (%c)\n", j++,a[i]);
  else
 {
- for(i=3;i < strlen(a);i++)
+ for(i=2;i < strlen(a);i++)
  {
  if(isdigit(a[i]))
  {
@@ -34,7 +34,7 @@ if(islower( a[i]))
  printf("lw $t%d, (%c)\n", j++,a[i]);
  else
  {
- for(i=3;i < strlen(a);i++)
+ for(i=2;i < strlen(a);i++)
  {
  if(isdigit(a[i]))
  {
@@ -54,12 +54,13 @@ if(islower( a[i]))
  else if( a[i] == '/')
  printf("div $t%d, $t%d, $t%d\n", j,j-2,j-1);
  }
- else if(strlen(a)==4) {
- i=i+3;
+
+ else if(strlen(a)==3) {
+ i=i+2;
  if( islower(a[i]))
 {
  printf("lw $t%d, %c\n",j,a[i]);
- printf("move %c, $t%d\n", a[i-3],j);
+ printf("copy %c, $t%d\n", a[i-2],j);
 }
  else
  printf("li $t%d, %c\n",j,a[i]);
